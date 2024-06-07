360141395a171cf57bec41e3e453f4ca26679287
Fabi-Chan <fabi@fabi-chan.me>
2024-06-07 12:31:08 +0200

    .

--- src/main/resources/dbmigrations/V1.0.0__Init.sql
+++ src/main/resources/dbmigrations/V1.0.0__Init.sql
@@ -1,11 +1,10 @@
 set schema 'public';
 
 
-create table qoutestats
-(
-    id         serial primary key,
-    user_id    bigint not null,
+create table qoutestats (
+    id serial primary key,
+    user_id bigint not null,
     channel_id bigint not null,
-    guild_id   bigint not null,
-    timestamp  bigint not null
+    guild_id bigint not null,
+    timestamp bigint not null
 );
