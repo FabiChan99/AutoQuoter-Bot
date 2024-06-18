df19eb12cb48b85023a2dea723a7c73f8180f743
Fabi-Chan <fabi@fabi-chan.me>
2024-06-18 14:46:30 +0200

    Refactor GuildCrosspostCommand in Kotlin code
    The commit simplifies the GuildCrosspostCommand class by removing unnecessary import statements and white spaces. It also enhances the readability of the code by breaking a lengthy line into two. These changes help in increasing the overall maintainability of the codebase.

--- src/main/kotlin/me/fabichan/autoquoter/commands/GuildCrosspostCommand.kt
+++ src/main/kotlin/me/fabichan/autoquoter/commands/GuildCrosspostCommand.kt
@@ -1,7 +1,6 @@
 package me.fabichan.autoquoter.commands
 
 import dev.minn.jda.ktx.coroutines.await
-import dev.minn.jda.ktx.messages.Embed
 import io.github.freya022.botcommands.api.commands.annotations.Command
 import io.github.freya022.botcommands.api.commands.application.provider.GlobalApplicationCommandManager
 import io.github.freya022.botcommands.api.commands.application.provider.GlobalApplicationCommandProvider
@@ -9,27 +8,20 @@ import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashE
 import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.db.Database
 import io.github.freya022.botcommands.api.core.db.preparedStatement
-import me.fabichan.autoquoter.config.Config
-import net.dv8tion.jda.api.JDAInfo
 import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.interactions.components.buttons.Button
-import net.dv8tion.jda.api.requests.RestAction
-import net.dv8tion.jda.api.utils.TimeFormat
-import java.lang.management.ManagementFactory
-import kotlin.time.Duration.Companion.minutes
 
 @Command
 class GuildCrosspostCommand(
     private val context: BContext,
     private val database: Database
 
-    ) : GlobalApplicationCommandProvider {
+) : GlobalApplicationCommandProvider {
     suspend fun onCommand(
         event: GuildSlashEvent,
         enable: Boolean
     ) {
         event.deferReply().await()
-        
+
         if (isCrossGuildPostingEnabled(event.guild.id) == enable) {
             event.hook.sendMessage("Cross-guild posting is already ${if (enable) "enabled" else "disabled"}.").queue()
             return
@@ -37,13 +29,14 @@ class GuildCrosspostCommand(
 
         if (enable) {
             setGuildCrosspost(event.guild.id, true)
-            event.hook.sendMessage("Cross-guild posting has been enabled. **You accept that messages from this guild may be quoted in other guilds if the user has access to a message link.**").queue()
+            event.hook.sendMessage("Cross-guild posting has been enabled. **You accept that messages from this guild may be quoted in other guilds if the user has access to a message link.**")
+                .queue()
         } else {
             setGuildCrosspost(event.guild.id, false)
             event.hook.sendMessage("Cross-guild posting has been disabled.").queue()
         }
-        
-        
+
+
     }
 
     override fun declareGlobalApplicationCommands(manager: GlobalApplicationCommandManager) {
@@ -53,18 +46,18 @@ class GuildCrosspostCommand(
             userPermissions += Permission.MANAGE_SERVER
             botPermissions += Permission.MESSAGE_SEND
             botPermissions += Permission.MESSAGE_EMBED_LINKS
-            option("enable", "enabled"){
+            option("enable", "enabled") {
                 description = "Enables or Disable (True/False) cross-guild posting."
             }
         }
     }
-    
+
     private suspend fun setGuildCrosspost(guildId: String, crosspost: Boolean) {
         database.preparedStatement("UPDATE guildsettings SET crossguildposting = ? WHERE guild_id = ?") {
             executeUpdate(crosspost, guildId.toLong())
         }
     }
-    
+
     private suspend fun isCrossGuildPostingEnabled(guildid: String): Boolean {
         return database.preparedStatement("SELECT crossguildposting FROM guildsettings WHERE guild_id = ?") {
             executeQuery(guildid.toLong()).map { it.getBoolean("crossguildposting") }.firstOrNull() ?: false
